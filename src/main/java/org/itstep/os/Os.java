5dbacba4a7634edefedfdb4292b00c0443cbd85b
gk <demar.demidov@yandex.ru>
2024-08-26 19:31:02 +0300

    test

--- src/main/java/org/itstep/os/Os.java
+++ src/main/java/org/itstep/os/Os.java
@@ -3,10 +3,10 @@ package org.itstep.os;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @Entity
