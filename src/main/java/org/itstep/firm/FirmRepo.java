3725f91406400ab50333952b4f54f4b69619c492
gk <demar.demidov@yandex.ru>
2024-08-26 19:00:23 +0300

    test

---
+++ src/main/java/org/itstep/firm/FirmRepo.java
@@ -0,0 +1,7 @@
+package org.itstep.firm;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FirmRepo extends JpaRepository<Firm,Long> {
+
+}
---
+++ src/main/java/org/itstep/os/OsRepo.java
@@ -0,0 +1,7 @@
+package org.itstep.os;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OsRepo extends JpaRepository<Os,Long> {
+
+}
